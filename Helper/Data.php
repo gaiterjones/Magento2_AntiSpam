812e4354c6a7c591cc846f5147b7aad96cd2b9b8
gaiterjones <paj@gaiterjones.com>
2024-07-25 15:45:40 +0200

    Improve false positives

--- Helper/Data.php
+++ Helper/Data.php
@@ -12,6 +12,9 @@ use Magento\Framework\App\Helper\AbstractHelper;
 class Data extends AbstractHelper
 {
 
+    const MIN_SUBSTRING_LENGTH = 4; // Minimum length of substring to consider it suspicious
+    const MAX_LEVENSHTEIN_DISTANCE = 2; // Maximum allowed Levenshtein distance
+
     /**
      * @param \Magento\Framework\App\Helper\Context $context
      */
@@ -21,16 +24,24 @@ class Data extends AbstractHelper
         parent::__construct($context);
     }
 
-    public function isSpamRegistration($firstName, $lastName) {
+    public function isSpamRegistration($firstName, $lastName)
+    {
         // Convert both names to lower case to make the check case-insensitive
         $firstNameLower = strtolower($firstName);
         $lastNameLower = strtolower($lastName);
-    
-        // Check if the first name is contained within the last name
-        if (strpos($lastNameLower, $firstNameLower) !== false) {
+
+        // Check if the first name is contained within the last name with a minimum length
+        if (strlen($firstNameLower) >= self::MIN_SUBSTRING_LENGTH &&
+            strpos($lastNameLower, $firstNameLower) !== false &&
+            (strpos($lastNameLower, $firstNameLower) !== 0 && strpos($lastNameLower, $firstNameLower) + strlen($firstNameLower) != strlen($lastNameLower))) {
             return true; // Spam detected
         }
-        
+
+        // Check Levenshtein distance between first name and last name
+        if (levenshtein($firstNameLower, $lastNameLower) <= self::MAX_LEVENSHTEIN_DISTANCE) {
+            return true; // Spam detected
+        }
+
         return false; // No spam detected
     }
 }
